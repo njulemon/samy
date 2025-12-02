1d134714ef7dfacfa64300f5680ead4d06d3935b
Nicolas Julémont <nicolas.julemont@gmail.com>
2025-12-02 10:58:47 +0100

    Version 1.5.0 - updated Node to 20. - updated Django to 6.0 Still need to fix sass (scss) issues with bootstrap.

--- react-ui-old/src/Tools/String.ts
+++
@@ -1,9 +0,0 @@
-export function capitalize(string_value: string): string {
-    if (typeof string_value === 'string') {
-        return string_value.replace(/^./, string_value[0].toUpperCase());
-    }
-    else {
-        return ""
-    }
-
-}
--- react-ui-old/src/hooks/useVotesHook.js
+++
@@ -1,4 +0,0 @@
-export const useVotesHook = () => {
-    // TODO
-    return []
-}
